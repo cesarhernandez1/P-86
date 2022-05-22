6599c7f65cf16b6ae6a20a959d3c7d5cca163eef
cesarhernandez1 <93447086+cesarhernandez1@users.noreply.github.com>
2022-05-22 11:00:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.Image.fromURL('BirthdayImage.jpg', function(Img) {
+	
+}
+
+function playSound(){
+	
+}
